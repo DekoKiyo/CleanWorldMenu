94fcdb9313da5cbc5eff816e809f914c17dbd947
DekoKiyo <dekobokokiyomori0817@gmail.com>
2024-01-06 00:43:56 +0900

    Fix the issue that crash when all values are false

--- CleanWorldMenu/CleanWorld.cs
+++ CleanWorldMenu/CleanWorld.cs
@@ -6,6 +6,7 @@ internal static class CleanWorld
 {
     internal static void ClearWorld(bool livingPeds = true, bool deadPeds = true, bool livingVehicles = true, bool deadVehicles = true, bool persistentEntities = true, bool reloadWorld = true)
     {
+        if (!livingPeds && !deadPeds && !livingVehicles && !deadVehicles && !persistentEntities && !reloadWorld) livingPeds = true;
         World.CleanWorld(livingPeds, deadPeds, livingVehicles, deadVehicles, persistentEntities, reloadWorld);
     }
 }
